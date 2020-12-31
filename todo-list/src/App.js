ad7815cf47f23d7fef2ee66ea2f9984a2260cb2a
Alaa Shurrab <alaa.m.shurrab98@gmail.com>
2020-12-31 22:05:28 +0200

    adding the onClick event handler for the add button & adding a new state key whish is the array that contains the data for the todos & creating the addTodo function which use an event to get to the input to get its value and then ad it to the statearray

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -2,6 +2,32 @@ import "./App.css";
 import React from "react";
 
 class App extends React.Component {
+  state = {
+    todoArr: [],
+  };
+
+  addTodo = (event) => {
+    if (this.state.todoArr.length >= 10) {
+      return alert("limit reched");
+    }
+    if (!event.target.parentNode.firstChild.value.trim()) {
+      return;
+    }
+    this.setState(
+      (pastState) => {
+        return {
+          todoArr: [
+            ...pastState.todoArr,
+            event.target.parentNode.firstChild.value,
+          ],
+        };
+      },
+      () => {
+        event.target.parentNode.firstChild.value = "";
+      }
+    );
+  };
+
   render() {
     return (
       <div className="App App-header">
@@ -11,16 +37,24 @@ class App extends React.Component {
             id="intary"
             name="intary"
             placeholder="write what to do"
+            autocomplete="off"
             className="text-field"
           />
-          <button className="add-btn" type="button" name="add">
+          <button
+            className="add-btn"
+            type="button"
+            name="add"
+            onClick={this.addTodo}
+          >
             Add
           </button>
           <button className="reset-btn" type="button" name="Reset">
             Reset
           </button>
 
-          <div className="todo-container"></div>
+          <div className="todo-container">
+            {/* should add the todo component here */}
+          </div>
           <h1 className="title">To-Do Lest</h1>
         </div>
       </div>
